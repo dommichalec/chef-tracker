094ea59e2985a28f018f58d411936f37da93b1b3
Dominic Michalec <dominicjjmichalec@gmail.com>
2017-07-05 17:01:48 -0400

    end of lesson one

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,15 @@
 // ember g controller application
 import Ember from 'ember';
 
+// extend the Ember object {key: value}
 export default Ember.Controller.extend({
-  name: "chef"
+  name: "chef",
+  chefs: [
+    {name: "Gordon Ramsey", isAvailable: true},
+    {name: "Mario Batalli", isAvailable: true},
+    {name: "Emerald Legassi", isAvailable: false},
+    {name: "Martha Stewart", isAvailable: false},
+    {name: "Giada Denalova", isAvailable: true},
+  ],
+  restaurant: {name: "Bertoli's", age: 3, location: "Columbus"},
 });
