fd1e7f53f35bf757169f11ee70bbdd8e5c8d16d5
Dominic Michalec <dominicjjmichalec@gmail.com>
2017-07-05 20:01:02 -0400

    finish up to css

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -11,5 +11,12 @@ export default Ember.Controller.extend({
     {name: "Martha Stewart", isAvailable: false},
     {name: "Giada Denalova", isAvailable: true},
   ],
+  dishes: [
+    {name: "Chicken Parm", isAvailable: true},
+    {name: "Spaghetti and Meatballs", isAvailable: false},
+    {name: "Veal Parm", isAvailable: true},
+    {name: "Chicken Alfredo", isAvailable: false},
+    {name: "Ceasar Salad", isAvailable: true},
+  ],
   restaurant: {name: "Bertoli's", age: 3, location: "Columbus"},
 });
